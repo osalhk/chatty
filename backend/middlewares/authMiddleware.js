b2c305d323e6821acdb0160e193dba92943e0766
Osal Geesara <geesarahko.23@uom.lk>
2026-03-22 01:30:17 +0530

    Add authentication middleware and routes for user management

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
+const authMiddleware = async (req, res, next) => {
+
+    try {
+        const token = req.cookies.jwt;
+
+        if (!token) {
+
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = await User.findById(decoded.userId).select("-password");
+
+        next();
+
+    }
+    catch (error) {
+        console.error(error);
+        res.status(401).json({ message: "Unauthorized" });
+    }
+
+}
+
+export default authMiddleware;
