80d7c0463ac038f9e4a7898598a5bfcd48ab4cfc
Osal Geesara <geesarahko.23@uom.lk>
2026-03-22 00:21:56 +0530

    Add login and getuser methods to AuthenticationController with JWT authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,23 +1,25 @@
 import bycrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 import User from "../models/User.js"
+import generateUniqueConnectCode from "../utils/generateUniqueConnectCode.js"
 
-class AuthenticationController{
-    static async register(req,res){
-        try{
-            const{fullName, username, email, password} = req.body;
+class AuthenticationController {
+    static async register(req, res) {
+        try {
+            const { fullName, username, email, password } = req.body;
 
-            if(!fullName || !username || !email || !password){
-                return res.status(400).json({message: "All fields are required"})
+            if (!fullName || !username || !email || !password) {
+                return res.status(400).json({ message: "All fields are required" })
             }
 
-            if (password.length < 6){
-                return res.status(400).json({message: "Password must be at least 6 characters"})
+            if (password.length < 6) {
+                return res.status(400).json({ message: "Password must be at least 6 characters" })
             }
 
-            const existingUser = await User.findOne({$or: [{email}, {username}]})
-            ;
-            if(existingUser){
-                return res.status(400).json({message: "Email or username already in use"})
+            const existingUser = await User.findOne({ $or: [{ email }, { username }] })
+                ;
+            if (existingUser) {
+                return res.status(400).json({ message: "Email or username already in use" })
             }
 
             const salt = await bycrypt.genSalt(10);
@@ -40,4 +42,77 @@ class AuthenticationController{
             res.status(500).json({ message: "Internal server error" });
         }
     }
+
+    static async login(req, res) {
+        try {
+            const { email, password } = req.body;
+
+            const user = await User.findOne({ email });
+            if (!email || !password) {
+                return res.status(400).json({ message: "Email and password are required" })
+            }
+            if (!user) {
+                return res.status(400).json({ message: "Invalid credentials" });
+            }
+
+            const isMatch = await bycrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(400).json({ message: "Invalid credentials" });
+            }
+
+            const token = jwt.sign(
+                { userId: user._id, email: user.email },
+                process.env.JWT_SECRET,
+                { expiresIn: "7d" }
+            );
+
+
+            res.cookie("jwt", token, {
+                maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+                httpOnly: true,
+                sameSite: "strict",
+                secure: process.env.NODE_ENV === "development",
+            });
+
+            res.status(200).json({
+                user: {
+                    id: user.id,
+                    username: user.username,
+                    fullName: user.fullName,
+                    email: user.email,
+                    connectCode: user.connectCode,
+                },
+
+            });
+
+        } catch (error) {
+            console.error("Login error", error);
+            res.status(500).json({ message: "Internal server error" });
+        }
+    }
+
+    static async me(req, res) {
+        try {
+            const user = await User.findById(req.user.userId).select("-password");
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            res.status(200).json({
+                user: {
+                    id: user.id,
+                    username: user.username,
+                    fullName: user.fullName,
+                    email: user.email,
+                    connectCode: user.connectCode,
+                },
+            });
+
+        } catch (error) {
+            console.error("me error", error);
+            res.status(500).json({ message: "Internal server error" });
+        }
+    }
 }
+
+export default AuthenticationController;
