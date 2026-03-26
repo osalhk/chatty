307d6cb64f41b7305c9d7fba152f8f74eaaba78b
Osal Geesara <geesarahko.23@uom.lk>
2026-03-26 21:14:09 +0530

    fix: correct layout and spacing issues in Auth component

--- frontend/src/pages/Auth/Auth.tsx
+++ frontend/src/pages/Auth/Auth.tsx
@@ -9,25 +9,27 @@ const Auth: React.FC = () => {
     return <>
         <div className="min-h-screen w-full bg-white flex flex-col md:flex-row">
             <div className="w-full md:w-6/12 bg-sky-500 p-8 text-white flex flex-col justify-center">
-                <div className="text-center mb:8">
-                    <div className="bg-white/20 p-4 rounded-full">
-                        <Mail className="size-10" />
+                <div className="text-center mb-8">
+                    <div className="flex justify-center mb-6">
+                        <div className="bg-white/20 p-4 rounded-full">
+                            <Mail className="size-10" />
+                        </div>
                     </div>
+                    <h1 className="text-3xl font-bold mb-2">Welcome to Chatty</h1>
+                    <p>Connect with friends and family in real-time.</p>
                 </div>
-                <h1 className="text-3xl font-bold mb-2">Welcome to Chatty</h1>
-                <p>Connect with friends and family in real-time.</p>
                 <div className="mt-10 text-center">
                     <p className="text-sm opacity-80">Join thousand of users today!</p>
                 </div>
             </div>
-            <div className="w-full md:w-6/12 flex p-8 justify-center item-center ">
+            <div className="w-full md:w-6/12 flex p-8 justify-center items-center ">
                 {islogin ? (
                     <div className="w-full md:w-[400 px]">
-                        <LoginForm onSwitch={() =>setIsLogin(true)}/>
+                        <LoginForm onSwitch={() => setIsLogin(true)} />
                     </div>
                 ) : (
                     <div className="w-full md:w-[400 px]">
-                        <RegisterForm onSwitch={() =>setIsLogin(false)}/>
+                        <RegisterForm onSwitch={() => setIsLogin(false)} />
                     </div>
                 )}
             </div>
